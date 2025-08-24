f2a6a616924d5d8f5f7da0bac25d66c8108dde55
Danh Tran <cprmmb99@gmail.com>
2025-08-24 16:50:40 +0700

    Update new-train.py

--- src/new-train.py
+++ src/new-train.py
@@ -412,7 +412,7 @@ def load_checkpoint(unet, optimizer, lr_scheduler, args, accelerator):
     start_epoch = 0
     if args.resume_from_checkpoint:
         # Load model weights from a specific .pt file
-        checkpoint_file = args.resume_from_checkpoin # os.path.join(args.resume_from_checkpoint, "epoch_1.pt")  # Use epoch_1.pt or step_500.0.pt
+        checkpoint_file = args.resume_from_checkpoint # os.path.join(args.resume_from_checkpoint, "epoch_1.pt")  # Use epoch_1.pt or step_500.0.pt
         if os.path.exists(checkpoint_file):
             state_dict = torch.load(checkpoint_file, map_location='cpu')['model']
             result = unet.load_state_dict(state_dict, strict=args.strict)
