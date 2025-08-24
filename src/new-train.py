59607b95e99119bef0950ef2d4cc31bf29a70c27
Danh Tran <cprmmb99@gmail.com>
2025-08-24 16:38:55 +0700

    Update new-train.py

--- src/new-train.py
+++ src/new-train.py
@@ -413,7 +413,7 @@ def load_checkpoint(unet, optimizer, lr_scheduler, args, accelerator):
     if args.resume_from_checkpoint:
         # Load the full accelerator state from the checkpoint directory
         # accelerator.load_state(args.resume_from_checkpoint)
-        accelerator.load_state(os.path.basename(args.resume_from_checkpoint))
+        accelerator.load_state(os.path.dirname(args.resume_from_checkpoint))
         # Load model weights separately if needed
         checkpoint_file = args.resume_from_checkpoint # os.path.join(args.resume_from_checkpoint, f"{os.path.basename(args.resume_from_checkpoint)}.pt")
         if os.path.exists(checkpoint_file):
